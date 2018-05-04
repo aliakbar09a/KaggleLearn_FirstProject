bfcad33bad92fccb631d62ef3f9d46cd8223dd40
Ali Akbar <aliakbar09a@gmail.com>
2018-05-04 10:44:02 +0530

    X and Y added

--- Code/housing.py
+++ Code/housing.py
@@ -3,10 +3,21 @@ import pandas as pd
 main_file_path = '../data/train.csv'
 data = pd.read_csv(main_file_path)
 print(data.describe())
-#SalePrice column
+
+# SalePrice column
 SalePrice = data.SalePrice
 print(SalePrice.head())
-#two columns data
+
+# two columns data
 two_columns = ['GarageArea', 'YrSold']
 two_columns_data = data[two_columns]
 print(two_columns_data.describe())
+print(data.columns)
+# selecting the features for training
+features = ['MSSubClass', 'MSZoning', 'LotFrontage', 'LotArea', 'Street',
+       'Alley', 'LotShape', 'LandContour', 'Utilities', 'LotConfig',
+       'LandSlope', 'Neighborhood', 'Condition1', 'Condition2', 'BldgType',
+       'HouseStyle', 'OverallQual', 'OverallCond', 'YearBuilt', 'YearRemodAdd',
+       'RoofStyle', 'RoofMatl']
+X = data[features]
+y = data[['SalePrice']]
