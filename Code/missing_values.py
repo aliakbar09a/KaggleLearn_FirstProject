d55fceccf6c77032ce333fd961da523758ac5ef5
aliakbar09a <aliakbar09a@gmail.com>
2018-05-09 15:16:33 +0530

    missing values with imputer

--- Code/missing_values.py
+++ Code/missing_values.py
@@ -1,1 +1,13 @@
-import pandas an
+import pandas as pd
+data = pd.read_csv('../data/train.csv')
+ndata = data.select_dtypes(exclude= ['object'])
+
+col_with_missing = [col for col in ndata.columns
+                        if ndata[col].isnull().any()]
+
+for col in col_with_missing:
+    ndata[col + 'was_missing'] = ndata[col].isnull()
+
+from sklearn.preprocessing import Imputer
+imputer = Imputer()
+new_data = imputer.fit_transform(ndata)
